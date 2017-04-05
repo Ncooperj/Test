6daa336520db20e94b53d0542d97c9338cdc99b1
Ncooperj <ncooperj@gmail.com>
2017-04-05 00:22:19 -0500

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,26 @@
+import sys
+import random
+
+operations = True
+
+while operations:
+    op = input("Attak or Defense: ")
+
+    if op == a or op == A:
+        operator = random.randint(1,2)
+
+        if operator == 1:
+            print ("Rook")
+            
+        elif operator == 2:
+            print ("Sledge")
+
+    else o == d or D:
+        operator = random.randint(3,4)
+
+        if operator == 3:
+            print ('Smoke')
+            
+        elif opertor == 3:
+            print ('Twitch')
+                

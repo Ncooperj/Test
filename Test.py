be1dc3810989181fbbc5f66c5caf15fc8d7d4b92
Ncooperj <ncooperj@gmail.com>
2017-04-07 01:01:05 -0500

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,26 +1,35 @@
 import sys
 import random
 
-operations = True
-
-while operations:
+while True:
     op = input("Attak or Defense: ")
 
-    if op == a or op == A:
+    if op == "a" or op == "A":
         operator = random.randint(1,2)
 
         if operator == 1:
-            print ("Rook")
-            
-        elif operator == 2:
-            print ("Sledge")
+            print("Glaz")
 
-    else o == d or D:
-        operator = random.randint(3,4)
+        elif operator == 2:
+            print("Sledge")
+
+    elif op == "d" or op == "D":
+        operator = random.randint(3,7)
 
         if operator == 3:
-            print ('Smoke')
-            
-        elif opertor == 3:
-            print ('Twitch')
-                
+            print("Smoke")
+
+        elif operator == 4:
+            print("Twitch")
+
+        elif operator == 5:
+            print("Jager")
+
+        elif operator == 6:
+            print("Mira")
+
+        elif operator == 7:
+            print("Rook")
+
+    else:
+        print("Noob, choose Tachanka.")
